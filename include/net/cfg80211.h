RobertRozic/android_kernel_samsung_s3ve3g

cfg80211: Add wiphy flag to disable 11D hints
Original commit:
https://github.com/CyanogenMod/android_kernel_motorola_msm8226/commit/da4d82de71d28ef1c78b4090f2feaba0c33a77f6

include/net/cfg80211.h
@@ -1864,6 +1864,9 @@ struct cfg80211_ops {
  *	responds to probe-requests in hardware.
  * @WIPHY_FLAG_OFFCHAN_TX: Device supports direct off-channel TX.
  * @WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL: Device supports remain-on-channel call.
+ * @WIPHY_FLAG_DISABLE_11D_HINT_FROM_CORE: enable this flag if the driver
+ *	does not want the channel flags to be changed by the country IE of the
+ *	connected BSS due to a regulatory hint from the core
  */
 enum wiphy_flags {
 	WIPHY_FLAG_CUSTOM_REGULATORY		= BIT(0),
@@ -1887,6 +1890,7 @@ enum wiphy_flags {
 	WIPHY_FLAG_AP_PROBE_RESP_OFFLOAD	= BIT(19),
 	WIPHY_FLAG_OFFCHAN_TX			= BIT(20),
 	WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL	= BIT(21),
+	WIPHY_FLAG_DISABLE_11D_HINT_FROM_CORE	= BIT(22),
 };
 
 /**

Jackeagle/caf_kernel_lollipop

include/net/cfg80211.h
@@ -2386,6 +2386,9 @@ struct cfg80211_ops {
  * @WIPHY_FLAG_OFFCHAN_TX: Device supports direct off-channel TX.
  * @WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL: Device supports remain-on-channel call.
  * @WIPHY_FLAG_DFS_OFFLOAD: The driver handles all the DFS related operations.
+ * @WIPHY_FLAG_DISABLE_11D_HINT_FROM_CORE: enable this flag if the driver
+ *	does not want the channel flags to be changed by the country IE of the
+ *	connected BSS due to a regulatory hint from the core
  */
 enum wiphy_flags {
 	WIPHY_FLAG_CUSTOM_REGULATORY		= BIT(0),
@@ -2409,7 +2412,8 @@ enum wiphy_flags {
 	WIPHY_FLAG_AP_PROBE_RESP_OFFLOAD	= BIT(19),
 	WIPHY_FLAG_OFFCHAN_TX			= BIT(20),
 	WIPHY_FLAG_HAS_REMAIN_ON_CHANNEL	= BIT(21),
-	WIPHY_FLAG_DFS_OFFLOAD                  = BIT(22)
+	WIPHY_FLAG_DFS_OFFLOAD                  = BIT(22),
+	WIPHY_FLAG_DISABLE_11D_HINT_FROM_CORE	= BIT(22)
 };
 
 /**